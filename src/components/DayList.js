e0d4fdce12680bd0ceafd6c937632658881910b6
houseofsam <samxsaid@gmail.com>
2022-01-18 21:40:00 -0500

    Create DayList component, write stories, import DayListItems and map over them to output in DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+const DayList = (props) => {
+  const DayListItems = props.days.map((day) => 
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  );
+
+  return (
+    <ul>{DayListItems}</ul>
+  )
+}
+
+export default DayList
