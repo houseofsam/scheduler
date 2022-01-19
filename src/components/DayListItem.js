e36e03a362c858116d6fdf03d29a574106bcca1f
houseofsam <samxsaid@gmail.com>
2022-01-18 21:38:35 -0500

    Create DayListItem component for individual days, write storybook stories for component, write unit tests.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
+import classNames from 'classnames';
+import './DayListItem.scss';
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spot === 0
+  });
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return 'no spots remaining';
+    } else if (spots === 1) {
+      return '1 spot remaining';
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
