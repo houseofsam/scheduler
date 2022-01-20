8b261f1bf9fdc3d48e6a8f756f8e9d7a12c4d4ec
houseofsam <samxsaid@gmail.com>
2022-01-20 02:04:50 -0500

    Prevent form default behaviour upon submit and modify cancel button onClick handler

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,11 +6,20 @@ const Form = (props) => {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [student, setStudent] = useState(props.student || "");
 
+  const reset = () => {
+    setInterviewer("");
+    setStudent("");
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -28,8 +37,8 @@ const Form = (props) => {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave()}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
