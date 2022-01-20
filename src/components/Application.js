aca7a594c840ecf3617c2593206df7471e8afd2b
houseofsam <samxsaid@gmail.com>
2022-01-20 03:09:00 -0500

    Import Appointment component to Application.js and render using mock data
    Ensure last appointment has a time of 5pm and a key of last representing the last appointment for the day

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import Button from './Button';
 import DayList from './DayList';
+import Appointment from './Appointment';
 
 const days = [
   {
@@ -22,8 +23,51 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  
+  const appointmentList = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ))
 
   return (
     <main className="layout">
@@ -48,6 +92,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
         <Button>Default</Button>
       </section>
     </main>
