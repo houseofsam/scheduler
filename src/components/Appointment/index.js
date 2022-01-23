cba56e9c3f18636a9190ae300feb3f8cbde59943
houseofsam <samxsaid@gmail.com>
2022-01-22 20:13:04 -0500

    Implement transition to form component and backing out when cancel button on form is clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,26 +4,30 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = (props) => {
 
   const { mode, transition, back } = useVisualMode(
+    // Initial state argument
     props.interview ? SHOW : EMPTY
   );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   )
 };
